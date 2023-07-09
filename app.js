293e10b4c4324626a34e9bbc0e2eb83fd98488c7
Abhishek Kashyap <abhi3566@gmail.com>
2023-07-09 16:22:40 +0530

    env

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ dotenv.config();
 const db = process.env.MONGO_URI;
 
 //------Connect to Mongo--------//
-mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(db)
     .then(() => console.log("Connected to MongoDB successfully!"))
     .catch(err => console.log(err));
 
